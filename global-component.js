e98e31d129dc1ed54112d6e9a819d05aa56b841a
vaoviec-dev <designer.duytrung@gmail.com>
2026-06-02 16:20:06 +0700

    update

--- global-component.js
+++ global-component.js
@@ -1,26 +1,23 @@
 // 1. HEADER CHUNG
 class GlobalHeader extends HTMLElement {
     connectedCallback() {
-        const depth = window.location.pathname.split('/').filter(Boolean).length;
-        const basePath = depth > 1 ? '../' : './';
-
         this.innerHTML = `
         <header class="header">
             <div class="header__overlay js-menu-overlay"></div>
             <div class="grid wide">
                 <div class="row header__wrapper">
-                    <div class="header__logo-col"><a href="${basePath}index.html" class="header__logo">D</a></div>
+                    <div class="header__logo-col"><a href="/" class="header__logo">D</a></div>
                     <nav class="header__nav-col">
                         <div class="header__close-btn js-menu-close"><i class="fas fa-times"></i></div>
                         <ul class="header__menu">
-                            <li><a href="${basePath}index.html" class="header__link">Home</a></li>
-                            <li><a href="${basePath}index.html#about" class="header__link">About me</a></li>
-                            <li><a href="${basePath}index.html#experiences" class="header__link">Experience</a></li>
-                            <li><a href="${basePath}index.html#portfolio" class="header__link">Portfolio</a></li>
+                            <li><a href="/" class="header__link">Home</a></li>
+                            <li><a href="/#about" class="header__link">About me</a></li>
+                            <li><a href="/#experiences" class="header__link">Experience</a></li>
+                            <li><a href="/#portfolio" class="header__link">Portfolio</a></li>
                         </ul>
                     </nav>
                     <div class="header__actions-col">
-                        <a href="${basePath}index.html#contact" class="btn header__btn-contact">Contact Me</a>
+                        <a href="/#contact" class="btn header__btn-contact">Contact Me</a>
                         <div class="header__toggle-btn js-menu-open"><i class="fas fa-bars"></i></div>
                     </div>
                 </div>
@@ -44,27 +41,23 @@ class GlobalHeader extends HTMLElement {
     }
 }
 
-// 2. FOOTER CHUNG (Style Mẫu Mới)
+// 2. FOOTER CHUNG
 class GlobalFooter extends HTMLElement {
     connectedCallback() {
-        const depth = window.location.pathname.split('/').filter(Boolean).length;
-        const basePath = depth > 1 ? '../' : './';
-
         this.innerHTML = `
         <footer class="footer">
             <div class="grid wide">
                 <div class="footer__content">
-                    <a href="${basePath}index.html" class="footer__logo">D</a>
+                    <a href="/" class="footer__logo">D</a>
                     <nav class="footer__nav">
                         <ul class="footer__menu">
-                            <li><a href="${basePath}index.html">Home</a></li>
-                            <li><a href="${basePath}index.html#about">About me</a></li>
-                            <li><a href="${basePath}index.html#experiences">Experience</a></li>
-                            <li><a href="${basePath}index.html#portfolio">Portfolio</a></li>
-                            <li><a href="${basePath}index.html#contact">Contact me</a></li>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/#about">About me</a></li>
+                            <li><a href="/#experiences">Experience</a></li>
+                            <li><a href="/#portfolio">Portfolio</a></li>
+                            <li><a href="/#contact">Contact me</a></li>
                         </ul>
                     </nav>
-                    
                     <div class="footer__bottom">
                         <p>Designed by @duytrung Graphic Designer</p>
                     </div>
